906f92215072685c3ea5b107f1c8be8203c809b5
Rounaq <rounaqwl66@gmail.com>
2015-05-11 09:00:56 +0530

    test home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
 
   get 'static_pages/directory'
 
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   get 'static_pages/feedback'
 
-  root 'users#index'
+  get 'users/index'
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
