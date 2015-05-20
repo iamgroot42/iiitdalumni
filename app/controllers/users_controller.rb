f12bb8e91374c2e16cdf3d5d914c2b607fd48294
Rounaq <rounaqwl66@gmail.com>
2015-05-20 11:06:59 +0530

    Post Model, user fields add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,10 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   def index
   end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :username
+  end
 end
---
+++ app/views/posts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @post, :id, :content, :user_id, :title, :image_url, :created_at, :updated_at
