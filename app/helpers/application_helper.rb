17263ec1364e2c81a436d43b992ca4aa664f962b
Rounaq <rounaqwl66@gmail.com>
2015-05-21 15:09:25 +0530

    Notification Temp.  Fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+	def alertSanitizer(s)
+		return "info" if s == "notice"
+		return "success" if s == "success"
+		return "warning" if s == "alert"
+		return "danger"
+	end
 end
