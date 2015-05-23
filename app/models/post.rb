8b4e42bea5daef3976afa9766494536758628a6f
Rounaq <rounaqwl66@gmail.com>
2015-05-23 13:04:15 +0530

    Buttons fix, Image Size fix, provision for default blog image, tested pagination.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,9 @@ class Post < ActiveRecord::Base
   belongs_to :user
   validates :title, presence: true
   validates :content, presence: true, length: {minimum: 10}
+  before_save :set_pic
+
+  def set_pic
+    self.image_url = "blog-placeholder.jpg" unless self.image_url.present?
+  end 
 end
