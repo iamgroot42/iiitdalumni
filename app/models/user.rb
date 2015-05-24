a792ffa347e5c7aa4fc5d74e832d080fe69fb30e
Rounaq <rounaqwl66@gmail.com>
2015-05-24 18:35:13 +0530

    Checkboxes

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,6 @@ class User < ActiveRecord::Base
   validates :rollno, presence: true, uniqueness: true
   validates :phone, length: {is: 10} #Flawed
   validates :ld_url, :allow_blank => true, format: { with: /(ftp|http|https):\/\/?((www|\w\w)\.)?linkedin.com(\w+:{0,1}\w*@)?(\S+)(:([0-9])+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/ }
-  validates_presence_of   :avatar
-  validates_integrity_of  :avatar
   validates_processing_of :avatar
   has_many :posts
   devise :database_authenticatable, :registerable,
