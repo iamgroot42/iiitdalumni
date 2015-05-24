55a8b01b049e25de0b4f17c124e1ade146eec79a
Rounaq <rounaqwl66@gmail.com>
2015-05-24 19:22:55 +0530

    Post Pics

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,6 @@ class Post < ActiveRecord::Base
   belongs_to :user
   validates :title, presence: true
   validates :content, presence: true, length: { minimum: 10 }
-  
+  mount_uploader :avatar, AvatarUploader
+  validates_processing_of :avatar
 end
